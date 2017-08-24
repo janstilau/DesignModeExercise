e6ef5db551c23760738ff12ad205efa14f5ed54b
janstilau <janstilau@gmail.com>
2017-08-24 18:01:46 +0800

    sync

---
+++ DesignMode/State/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  State
+//
+//  Created by jansti on 2017/8/24.
+//  Copyright © 2017年 jansti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+// 状态模式的例子: 重构仓库的chapter_1 moive 根据 type 的不同计算金额功能.moive需要可以在运行时更改type, 每种type的运算方式也有不同.
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
