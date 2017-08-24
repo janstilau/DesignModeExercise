8a8516f5aed167c63e3d2e4071a8b3ca170949ae
justin lau <janstilau@gmail.com>
2017-08-25 00:03:28 +0800

    sync

---
+++ DesignMode/Composite/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Composite
+//
+//  Created by justin lau on 2017/8/25.
+//  Copyright © 2017年 jansti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
