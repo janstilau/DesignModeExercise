a429be5f0fc329533cc5d8427e3194b6775ecb63
justin lau <janstilau@gmail.com>
2017-08-30 00:18:43 +0800

    adapter

---
+++ DesignMode/Adapter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Adapter
+//
+//  Created by justin lau on 2017/8/29.
+//  Copyright © 2017年 jansti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// adapter, 就是用组合的方式实现对于接口的改写.
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
