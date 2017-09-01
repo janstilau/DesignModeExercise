896e71b209d3ff56911537cd4f766e752f1309ad
janstilau <janstilau@gmail.com>
2017-09-01 15:13:31 +0800

    sunc

---
+++ DesignMode/Prototype/Prototype.h
@@ -0,0 +1,18 @@
+//
+//  Prototype.h
+//  DesignMode
+//
+//  Created by jansti on 2017/9/1.
+//  Copyright © 2017年 jansti. All rights reserved.
+//
+
+#ifndef Prototype_h
+#define Prototype_h
+
+/*
+ 
+ 
+ */
+
+
+#endif /* Prototype_h */
