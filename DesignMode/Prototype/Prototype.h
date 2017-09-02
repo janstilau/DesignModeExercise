eebbf519bd7fd78dabce440881b667efead7f9d6
janstilau <janstilau@gmail.com>
2017-09-02 18:41:24 +0800

    sync

--- DesignMode/Prototype/Prototype.h
+++ DesignMode/Prototype/Prototype.h
@@ -10,7 +10,10 @@
 #define Prototype_h
 
 /*
- 
+ 陈硕
+ Prototype 的意义在于，你拿到一个 Base* ，它指向某个 Derived 对象，你想克隆出 Derived 对象，但代码中不写出 Derived 的具体类型，因为有很多派生类，这种情况下你用构造函数是搞不定的，type-switch 是 bad smells 。
+ 另外，这里考虑 virtual 的性能损失是主次不分，构造对象需要分配内存，这开销比一次虚函数调用大多了。
+ Prorobuf Message 就采用了 prototype 模式，你可以参考一下。
  
  */
 
