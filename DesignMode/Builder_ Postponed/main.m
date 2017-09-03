8e788d3ab08f5fe7a63ce5677a0d8cd8f00ef8b6
janstilau <janstilau@gmail.com>
2017-09-03 13:41:42 +0800

    sync

---
+++ DesignMode/Builder_ Postponed/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Builder_ Postponed
+//
+//  Created by jansti on 2017/9/3.
+//  Copyright © 2017年 jansti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
